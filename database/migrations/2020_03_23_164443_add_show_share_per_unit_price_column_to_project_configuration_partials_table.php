10c88d8c8fb3a35e21ef3ce70c1f3ba05a15c3e7
Captain Gopal <gopal@codentech.com>
2020-03-23 12:38:41 +0530

    added show_share_per_unit_price

---
+++ database/migrations/2020_03_23_164443_add_show_share_per_unit_price_column_to_project_configuration_partials_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddShowSharePerUnitPriceColumnToProjectConfigurationPartialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('project_configuration_partials', function (Blueprint $table) {
+            $table->boolean('show_share_per_unit_price')->default(1);
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('project_configuration_partials', function (Blueprint $table) {
+            $table->dropColumn('show_share_per_unit_price');
+        });    }
+}
