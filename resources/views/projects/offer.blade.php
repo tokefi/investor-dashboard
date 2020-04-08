405e9433a74961c7508357890f5ba4af3810dc7f
Captain Gopal <gopal@codentech.com>
2020-04-09 05:00:20 +0530

    fix the bug

--- resources/views/projects/offer.blade.php
+++ resources/views/projects/offer.blade.php
@@ -695,7 +695,7 @@ Offer Doc
 									@if($agent_investment)
 									@if($agent_investment == 1)
 									<input type="hidden" value="agent_investment" name="agent_investment">
-									<input type="hidden" value="{{$user->id}}" name="user_id">
+									<input type="hidden" @if(!Auth::guest()) value="{{$user->id}}" @endif name="user_id">
 									<input type="hidden" value="{{ $clientApplication->user_id }}" name="agent_id">
 									@endif
 									@endif
