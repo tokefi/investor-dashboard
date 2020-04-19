3d91f0dcbfd235ee5d185c61a2c6cc5ecf7b836f
Captain Gopal <gopal@codentech.com>
2020-04-20 02:51:08 +0530

    Added transaction column in user investment tab and couple of text correction

--- resources/views/users/investments.blade.php
+++ resources/views/users/investments.blade.php
@@ -47,6 +47,7 @@
 									{{-- <th>Investor Name</th> --}}
 									<th>Project Name</th>
 									<th>Investment Status</th>
+									<th>transaction type</th>
 									<th>Number of shares</th>
 									<th>Price</th>
 									<th>Amount</th>
@@ -55,31 +56,20 @@
 								</tr>
 							</thead>
 							<tbody>
-								@if($allTransactions->count())
-									@foreach($allTransactions as $allTransaction)
-										@if($allTransaction->transaction_type)
-											@if(($allTransaction->transaction_type == 'DIVIDEND') || ($allTransaction->transaction_type == 'ANNUALIZED DIVIDEND'))
-											<tr>
-												<td>@if($allTransaction->project->projectspvdetail){{$allTransaction->project->title}}@endif</td>
-												<td class="text-center">@if($allTransaction->transaction_type == 'DIVIDEND') DIVIDEND % FIXED @else DIVIDEND % ANNUALIZED @endif</td>
-												<td class="text-center">{{$allTransaction->number_of_shares}}</td>
-												<td>@if($allTransaction->transaction_type == 'DIVIDEND') ${{number_format(($allTransaction->rate/100), 4)}} @else ${{number_format($allTransaction->rate, 4)}} @endif</td>
-												<td>${{number_format($allTransaction->amount, 2)}}</td>
-												<td>@if($allTransaction->user->agent_id) <?php $agent= App\User::find($allTransaction->user->agent_id); ?> {{ $agent->first_name }} {{ $agent->last_name }} <br> {{ $allTransaction->user->agent_id }} @else NA @endif </td>
-												<td data-sort="{{date($allTransaction->transaction_date)}}">{{date('d/m/Y', strtotime($allTransaction->transaction_date))}}</td>
-											</tr>
-											@endif
-										@else
+								@if($transactions->count())
+									@foreach($transactions as $allTransaction)
+										
 										<tr>
-											<td>@if($allTransaction->project->projectspvdetail){{$allTransaction->project->title}}@endif</td>
+											<td>@if($allTransaction->project){{$allTransaction->project->title}}@endif</td>
 											<td class="text-center">@if($allTransaction->accepted && $allTransaction->money_received) Share Certificate Issued @elseif($allTransaction->money_received) Money Received @else Applied @endif</td>
+											<td>{{ $allTransaction->transaction_type }}</td>
 											<td class="text-center">{{round($allTransaction->amount)}}</td>
-											<td>${{number_format($allTransaction->buy_rate, 4)}}</td>
-											<td>${{number_format(round(($allTransaction->amount)*($allTransaction->buy_rate)), 2)}}</td>
+											<td>${{number_format($allTransaction->rate, 4)}}</td>
+											<td>${{number_format(($allTransaction->amount)*($allTransaction->rate))}}</td>
 											<td>@if($allTransaction->user->agent_id) <?php $agent= App\User::find($allTransaction->user->agent_id); ?> {{ $agent->first_name }} {{ $agent->last_name }} <br> {{ $allTransaction->user->agent_id }} @else NA @endif </td>
 											<td data-sort="{{date($allTransaction->created_at)}}">{{date('d/m/Y', strtotime($allTransaction->created_at))}}</td>
 										</tr>
-										@endif
+										
 									@endforeach
 								@endif
 							</tbody>
