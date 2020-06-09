b034f0e3ba4bd93541a8f0280c71764c4c1941a0
Captain Gopal <gopal@codentech.com>
2020-06-09 13:31:55 +0530

    added validation message

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -56,7 +56,7 @@ return [
     ],
     'not_in'               => 'The selected :attribute is invalid.',
     'numeric'              => 'The :attribute must be a number.',
-    'regex'                => 'The :attribute format is invalid.',
+    'regex'                => 'The :attribute format is alphanumeric only.',
     'required'             => 'The :attribute field is required.',
     'required_if'          => 'The :attribute field is required when :other is :value.',
     'required_with'        => 'The :attribute field is required when :values is present.',
