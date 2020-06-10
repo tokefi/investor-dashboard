8860e76785f7da56e23c5fb3f4d698adea9ed6c5
Captain Gopal <gopal@codentech.com>
2020-06-10 08:56:53 +0530

    added font color column

---
+++ database/migrations/2020_06_09_232407_add_column_font_color_to_colors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnFontColorToColorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('colors', function (Blueprint $table) {
+            $table->string('font_color');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('colors', function (Blueprint $table) {
+            $table->dropColumn('font_color');
+        }); 
+    }
+}
