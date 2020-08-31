8fd5cb378fb7a81aa265f73299243bb95085bc39
Captain Gopal <gopal@codentech.com>
2020-08-31 07:25:59 +0530

    fixed first custom field issue

--- resources/views/dashboard/configuration/partials/customFields.blade.php
+++ resources/views/dashboard/configuration/partials/customFields.blade.php
@@ -47,62 +47,60 @@
                             </div>
                         </form>
                     </div>
-                    <form>
-                        <div id="custom-fields-msg" class="hide alert alert-info text-center">Successfully update the custom fields!!!</div>
-                        @if(!$customFields->count())
-                        <div class="alert alert-info text-center">No custom fields created yet!</div>
-                        @endif
-                        <table class="table table-borderless table-striped table-responsive">
-                            <thead>
-                                <th>Type</th>
-                                <th>Section Type</th>
-                                <th>Field Label</th>
-                                <th>Is Required</th>
-                                <th>Attributes</th>
-                                <th>Properties</th>
-                                <th>Action</th>
-                            </thead>
-                            <tbody>
-                                @foreach ($customFields as $customField)
-                                <tr style="font-size: 0.9em;">
-                                    <td><select class="form-control" id="type" name="customFieldTypes[]" required style="border:0px solid !important ; background-color: transparent; box-shadow: inset 0 1px 1px rgba(0,0,0,0);    height: 22px; padding: 0px 12px;">
-                                        <option value="{{ $customField->type }}">{{ $customField->type }}</option>
-                                        <option value="text">Text</option>
-                                        <option value="date">Date</option>
-                                    </select>
-                                </td>
-                                <td>{{  $customField->applicationSection->label }}</td>
-                                <td><input type="text" name="customFieldLabels[]" value="{{ $customField->label }}" style="border:0px;  background-color: transparent;"> </td>
-                                <td class="text-center">
-                                    {{-- {{ $customField->is_required ? 'Yes' : 'No' }} --}}
-                                    <input type="checkbox" name='agent_type[]' class="toggle-elements" action="expected_return_{{ $customField->id }}" autocomplete="off"  data-size="mini" @if($customField->is_required) checked value="1" @else value="0" @endif id="expected_return_{{ $customField->id }}">
-                                </td>
-                                <td>
-                                    @if ($customField->attributes)
-                                    @foreach ($customField->attributes as $key => $attribute)
-                                    {{ $key }} : {{ $attribute == 'on' ? 'Yes' : $attribute }} <br />
-                                    @endforeach
-                                    @endif
-                                </td>
-                                <td>
-                                    @if ($customField->properties)
-                                    @foreach ($customField->properties as $key => $property)
-                                    {{ $key }} : {{ $property == 'on' ? 'Yes' :  $property}} <br />
-                                    @endforeach
-                                    @endif
-                                </td>
-                                <td class="text-center">
-                                    <form name="delete_custom_field" action="{{ route('custom-field.delete', [$customField->id]) }}" method="POST" style="cursor:pointer;">
-                                        {{ csrf_field() }}
-                                        <button type="submit" class="btn btn-danger btn-sm" title="Delete"><i class="fa fa-trash"></i></button>
-                                    </form>
-                                </td>
-                            </tr>
-                            <input type="hidden" name="customIds[]" value="{{ $customField->id }}">
-                            @endforeach
-                        </tbody>
-                    </table>
-                    <button type="submit" class="btn btn-primary pull-right" id="updateCustomField">Update</button>
-                </form>
+                    <div id="custom-fields-msg" class="hide alert alert-info text-center">Successfully update the custom fields!!!</div>
+                    @if(!$customFields->count())
+                    <div class="alert alert-info text-center">No custom fields created yet!</div>
+                    @endif
+                    <table class="table table-borderless table-striped table-responsive">
+                        <thead>
+                            <th>Type</th>
+                            <th>Section Type</th>
+                            <th>Field Label</th>
+                            <th>Is Required</th>
+                            <th>Attributes</th>
+                            <th>Properties</th>
+                            <th>Action</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($customFields as $customField)
+                            <tr style="font-size: 0.9em;">
+                                <td><select class="form-control" id="type" name="customFieldTypes[]" required style="border:0px solid !important ; background-color: transparent; box-shadow: inset 0 1px 1px rgba(0,0,0,0);    height: 22px; padding: 0px 12px;">
+                                    <option value="{{ $customField->type }}">{{ $customField->type }}</option>
+                                    <option value="text">Text</option>
+                                    <option value="date">Date</option>
+                                </select>
+                            </td>
+                            <td>{{  $customField->applicationSection->label }}</td>
+                            <td><input type="text" name="customFieldLabels[]" value="{{ $customField->label }}" style="border:0px;  background-color: transparent;"> </td>
+                            <td class="text-center">
+                                <input type="checkbox" name='agent_type[]' class="toggle-elements" action="expected_return_{{ $customField->id }}" autocomplete="off"  data-size="mini" @if($customField->is_required) checked value="1" @else value="0" @endif id="expected_return_{{ $customField->id }}">
+                            </td>
+                            <td>
+                                @if ($customField->attributes)
+                                @foreach ($customField->attributes as $key => $attribute)
+                                {{ $key }} : {{ $attribute == 'on' ? 'Yes' : $attribute }} <br />
+                                @endforeach
+                                @endif
+                            </td>
+                            <td>
+                                @if ($customField->properties)
+                                @foreach ($customField->properties as $key => $property)
+                                {{ $key }} : {{ $property == 'on' ? 'Yes' :  $property}} <br />
+                                @endforeach
+                                @endif
+                            </td>
+                            <td class="text-center">
+                                <form name="delete_custom_field" action="{{ route('custom-field.delete', [$customField->id]) }}" method="POST" style="cursor:pointer;">
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete"><i class="fa fa-trash"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                        <input type="hidden" name="customIds[]" value="{{ $customField->id }}">
+                        @endforeach
+                    </tbody>
+                </table>
+                <button type="submit" class="btn btn-primary pull-right" id="updateCustomField">Update</button>
+                
             </div>
         </section>
